82552347a685bb7284967ed080190fd4a54b4eee
@anulman <anulman@gmail.com>
2017-03-13 02:54:59 -0400

    feat(phantom): add lib-level phantom wrapper

---
+++ src/phantom.js
@@ -0,0 +1,44 @@
+import childProcess from 'child_process';
+import path from 'path';
+import phantomjs from 'phantomjs-prebuilt';
+
+export default class Phantom {
+  constructor({ args = [], bin = phantomjs.path } = {}) {
+    let script = path.join(__dirname, 'phantom', '-main.js');
+    let phantom = childProcess.spawn(bin, [].concat(args, [script]));
+
+    /* eslint-disable no-console */
+    phantom.stdout.on('data', (buffer) => {
+      console.log(buffer.toString() || 'could not read buffer');
+    });
+
+    phantom.stderr.on('data', (buffer) => {
+      console.error(buffer.toString() || 'could not read buffer');
+    });
+    /* eslint-enable no-console */
+
+    phantom.on('exit', () => {
+      this.phantom = null;
+    });
+
+    this.phantom = phantom;
+  }
+
+  startServer(options) {
+    this._exec({
+      command: 'startServer',
+      options
+    });
+
+    return options.port;
+  }
+
+  shutdown() {
+    this.phantom.stdin.end();
+    this.phantom.kill();
+  }
+
+  _exec(body = {}) {
+    this.phantom.stdin.write(`${JSON.stringify(body)}\n`, 'utf8');
+  }
+}
