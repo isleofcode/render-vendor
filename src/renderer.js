6c7ee5f14b887c41bbe88bf9f237180a9cb50eb9
@anulman <anulman@gmail.com>
2017-07-26 16:44:31 -0400

    feat(renderer): add core renderer type + test

--- src/renderer.js
+++ src/renderer.js
@@ -1,67 +1,211 @@
+import EventEmitter from 'events';
+
 import _ from 'lodash';
 import assert from 'assert';
-import request from 'request';
-import RSVP from 'rsvp';
 
-import Phantom from './phantom';
+import Page from './page';
+import makeErrorType from './utils/make-error-type';
 
-const { denodeify } = RSVP;
+const {
+  defaults,
+  isBoolean,
+  isString
+} = _;
 
-export default class Renderer {
-  constructor({ phantomOptions = {}, port, url, html } = {}) {
-    assert.ok(port, 'missing `port`');
-    assert.ok(url || html, 'missing either `url` or `html`');
+let defaultRenderer;
 
-    this.phantom = new Phantom(phantomOptions);
-    this.port = this.phantom.startServer({ port, url, html });
+export class Renderer extends EventEmitter {
+  static get rendererConstructor() {
+    return this._rendererConstructor || require('./renderers/phantom').default;
   }
 
-  render(options = {}) {
-    let renderJob = this._renderJob || this._render(options);
-    return renderJob;
+  static set rendererConstructor(value) {
+    this._rendererConstructor = value;
   }
 
-  _render(renderOptions) {
-    this._renderJob = this._exec({
-      command: 'render',
-      options: {
-        body: renderOptions
-      }
-    })
-      .then((response) => {
-        if (response.statusCode !== 303) {
-          let res = JSON.stringify(response);
-          throw new Error(`Renderer - Unexpected Response: ${res}`);
+  static get renderer() {
+    if (defaultRenderer === undefined) {
+      defaultRenderer = new this.rendererConstructor();
+    }
+
+    return defaultRenderer;
+  }
+
+  static set renderer(value) {
+    defaultRenderer = value;
+  }
+
+  constructor(attrs = {}) {
+    super();
+
+    this.pages = [];
+    defaults(this, attrs);
+  }
+
+  get isBooting() {
+    return this._isBooting || false;
+  }
+
+  set isBooting(value) {
+    assert.ok(isBoolean(value));
+    assert.ok(!this.isShuttingDown);
+
+    this._isBooted = false;
+    this._isBooting = value;
+  }
+
+  get isBooted() {
+    return this._isBooted || false;
+  }
+
+  set isBooted(value) {
+    assert.ok(isBoolean(value));
+
+    this._isBooting = false;
+    this._isBooted = value;
+    this._isShuttingDown = false;
+  }
+
+  get isShuttingDown() {
+    return this._isShuttingDown || false;
+  }
+
+  set isShuttingDown(value) {
+    assert.ok(isBoolean(value));
+    assert.ok(this.isBooted === value);
+
+    this._isShuttingDown = value;
+  }
+
+  get pages() {
+    return this._pages;
+  }
+
+  set pages(value) {
+    assert.ok(this._pages === undefined);
+    this._pages = value;
+  }
+
+  find(id) {
+    return this.pages.find((page) => page.id === id);
+  }
+
+  async load(id, options = {}) {
+    assert.ok(id !== undefined);
+
+    let page = this.find(id);
+
+    if (page === undefined) {
+      page = new Page({ id, renderer: this });
+      this.pages.push(page);
+    }
+
+    return page.load(options);
+  }
+
+  async refresh() {
+    try {
+      let pages = await Page.all({ renderer: this });
+
+      pages.forEach((_page) => {
+        let page = this.find(_page.id);
+
+        if (page === undefined) {
+          page = _page;
+          this.pages.push(page);
         }
 
-        return response.body;
-      })
-      .catch((err) => {
-        if (err instanceof Error) {
-          throw err;
-        } else {
-          throw new Error(`Renderer - RenderError: ${err}`);
-        }
-      })
-      .finally(() => {
-        this._renderJob = null;
+        page.isLoaded = true;
       });
-
-    return this._renderJob;
+    } catch (err) {
+      // nop
+    }
   }
 
-  shutdown() {
-    this.phantom.shutdown();
+  async destroy() {
+    if (!this.isBooted) {
+      return;
+    }
+
+    this.isShuttingDown = true;
+    this.removeAllListeners();
+
+    await this.shutdown();
+
+    this.pages.splice(0, this.pages.length);
+    this.isBooted = false;
   }
 
-  _exec({ command, options = {} }) {
-    assert.ok('command', 'missing command');
+  async _exec(command, page, ...args) {
+    assert.ok(isString(command));
+    assert.ok(page instanceof Page);
+    assert.ok(!page.isDestroyed);
+    assert.ok(!page.isDestroying || command === 'unload');
 
-    const post = denodeify(request.post);
-    let url = `http://localhost:${this.port}/${command}`;
+    if (!this.isBooted) {
+      await this.boot();
+    }
 
-    return post(url, _.defaults(options, {
-      json: true
-    }));
+    return this[`${command}Page`].call(this, page, ...args);
+  }
+
+  // MARK - abstract api
+  async boot() {
+    throw new Renderer.Error.NotImplemented();
+  }
+
+  async shutdown() {
+    throw new Renderer.Error.NotImplemented();
+  }
+
+  async ping() {
+    throw new Renderer.Error.NotImplemented();
+  }
+
+  async loadPage(page, ...args) {
+    throw new Renderer.Error.NotImplemented();
+  }
+
+  async renderPage(page, ...args) {
+    throw new Renderer.Error.NotImplemented();
+  }
+
+  async unloadPage(page, ...args) {
+    throw new Renderer.Error.NotImplemented();
+  }
+
+  // MARK - static API
+  static get isBooting() {
+    return this.renderer.isBooting;
+  }
+
+  static get isBooted() {
+    return this.renderer.isBooted;
+  }
+
+  static get pages() {
+    return this.renderer.pages;
+  }
+
+  static find() {
+    return this.renderer.find(...arguments);
+  }
+
+  static async load() {
+    return this.renderer.load(...arguments);
+  }
+
+  static async refresh() {
+    return this.renderer.refresh(...arguments);
+  }
+
+  static async destroy() {
+    return this.renderer.destroy(...arguments);
   }
 }
+
+export default Renderer;
+
+Renderer.Error = makeErrorType('RendererError');
+Renderer.Error.NotImplemented =
+  makeErrorType('NotImplementedError', Renderer.Error);
